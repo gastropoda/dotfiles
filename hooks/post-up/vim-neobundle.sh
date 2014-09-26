589d5a2dbe5bcc1c5f08411a3981a3589b9691d5
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-09-26 13:55:04 +0200

    vim: verbose neobundle bootstrap

--- hooks/post-up/vim-neobundle.sh
+++ hooks/post-up/vim-neobundle.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 if [ ! -e ~/.vim/bundle/neobundle.vim ]; then
+  echo "vim: bootstrapping neobundle"
+  echo
   mkdir -p ~/.vim/bundle
   git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+  echo
+  echo "vim: neobundle installed"
+  echo "vim: run vim to fetch all bundles"
 fi
