55d8a97102e6a7ba155ac6173a93e4b0dd99f6ad
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-09-27 20:52:15 +0200

    disable locales here

--- bashrc.d/locale.sh
+++ bashrc.d/locale.sh
@@ -1,9 +1,9 @@
 # software messages in American
-export LANG="en_US.UTF-8"
+#export LANG="en_US.UTF-8"
 # start week on Monday
-export LC_TIME="en_GB.UTF-8"
+#export LC_TIME="en_GB.UTF-8"
 # A4 in stead of Letter
-export LC_PAPER="en_GB.UTF-8"
+#export LC_PAPER="en_GB.UTF-8"
 # mm in stead of Inches
-export LC_MEASUREMENT="en_GB.UTF-8"
+#export LC_MEASUREMENT="en_GB.UTF-8"
 
