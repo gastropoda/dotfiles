f280fd6c521a729190d5f0f9be35cdc858e2fc59
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-09-27 21:11:00 +0200

    bash: check locales availablity

--- bashrc.d/locale.sh
+++ bashrc.d/locale.sh
@@ -1,9 +1,13 @@
-# software messages in American
-#export LANG="en_US.UTF-8"
-# start week on Monday
-#export LC_TIME="en_GB.UTF-8"
-# A4 in stead of Letter
-#export LC_PAPER="en_GB.UTF-8"
-# mm in stead of Inches
-#export LC_MEASUREMENT="en_GB.UTF-8"
+if locale -a | grep ^en_US.UTF-8 ; then
+  # software messages in American
+  export LANG="en_US.UTF-8"
+fi
 
+if locale -a | grep ^en_GB.UTF-8 ; then
+  # start week on Monday
+  export LC_TIME="en_GB.UTF-8"
+  # A4 in stead of Letter
+  export LC_PAPER="en_GB.UTF-8"
+  # mm in stead of Inches
+  export LC_MEASUREMENT="en_GB.UTF-8"
+fi
