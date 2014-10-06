013317c73192fb21e457602ba6d78ef064199657
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-10-06 09:07:11 +0200

    stringlist: support older bash versions

--- bash.lib/stringlist.sh
+++ bash.lib/stringlist.sh
@@ -1,8 +1,9 @@
 stringlist_append() {
-  declare -n stringlist=$1
-  appendage="$2"
+  local stringlist=${!1}
+  local appendage="$2"
   stringlist="${stringlist/$appendage ;/}"
   stringlist="${stringlist/; $appendage/}"
   stringlist="${stringlist/$appendage/}"
   stringlist="${stringlist:+$stringlist ;} $appendage"
+  typeset $1="$stringlist"
 }
