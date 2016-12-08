c159ca360a19cddca36e4efe4fe2390e4819849b
Artem Baguinski <abaguinski@depraktijkindex.nl>
2016-12-08 07:01:59 +0100

    join tmux session if the only

--- bashrc.d/zz_after/list_tmux_sessions.sh
+++ bashrc.d/zz_after/list_tmux_sessions.sh
@@ -1,9 +1,14 @@
 if [ -n "$PS1" -a -z "$TMUX" ] ; then
   if have_exe tmux ; then
     if ! have_exe pgrep || pgrep tmux > /dev/null ; then
-      echo "tmux sessions:"
-      tmux list-sessions | awk '{sub(/:$/,"",$1); print "tmux a -t", $1}'
+      lst=`tmux list-sessions`
+      cnt=`echo $lst | wc -l`
+      if [ "$cnt" = "1" ] ; then
+        tmux a
+      else
+        echo "tmux sessions:"
+        echo "$lst" | awk '{sub(/:$/,"",$1); print "tmux a -t", $1}'
+      fi
     fi
   fi
 fi
-
